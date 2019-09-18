c892ca5ed89af23cebd1405ee4a7ce3eeac90c44
Chaoyu Yang <paranoyang@gmail.com>
2019-09-17 17:39:53 -0700

    update h2o loan prediction notebook, marked as WIP

--- h2o/loan-prediction/loan_prediction.py
+++ h2o/loan-prediction/loan_prediction.py
@@ -1,14 +1,16 @@
 
+import h2o
+
 from bentoml import api, env, artifacts, BentoService
 from bentoml.artifact import H2oModelArtifact
 from bentoml.handlers import DataframeHandler
 
-import h2o
-
 @env(conda_pip_dependencies = ['h2o', 'pandas'])
 @artifacts([H2oModelArtifact('model')])
 class LoanPrediction(BentoService):
+    
     @api(DataframeHandler)
     def predict(self, df):
-        h2o_frame = h2o.H2OFrame(df)
-        return self.artifacts.model.predict(h2o_frame)
+        h2o_frame = h2o.H2OFrame(df, na_strings=['NaN'])
+        predictions = self.artifacts.model.predict(h2o_frame)
+        return predictions.as_data_frame()
