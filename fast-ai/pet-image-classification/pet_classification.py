dac65f2424b74c8c0375558248095c37018ab2f9
yubozhao <yubz86@gmail.com>
2020-07-19 20:54:41 -0700

    update notebooks to BentoML 0.8.2

--- fast-ai/pet-image-classification/pet_classification.py
+++ fast-ai/pet-image-classification/pet_classification.py
@@ -1,13 +1,13 @@
 
 from bentoml import BentoService, api, env, artifacts
 from bentoml.artifact import FastaiModelArtifact
-from bentoml.handlers import FastaiImageHandler
+from bentoml.adapters import FastaiImageInput
 
 @env(pip_dependencies=['fastai'])
 @artifacts([FastaiModelArtifact('pet_classifer')])
 class PetClassification(BentoService):
     
-    @api(FastaiImageHandler)
+    @api(input=FastaiImageInput())
     def predict(self, image):
         result = self.artifacts.pet_classifer.predict(image)
         return str(result)
--- h2o/loan-prediction/loan_prediction.py
+++ h2o/loan-prediction/loan_prediction.py
@@ -3,13 +3,13 @@ import h2o
 
 from bentoml import api, env, artifacts, BentoService
 from bentoml.artifact import H2oModelArtifact
-from bentoml.handlers import DataframeHandler
+from bentoml.adapters import DataframeInput
 
 @env(pip_dependencies = ['h2o', 'pandas'])
 @artifacts([H2oModelArtifact('model')])
 class LoanPrediction(BentoService):
     
-    @api(DataframeHandler)
+    @api(input=DataframeInput)
     def predict(self, df):
         h2o_frame = h2o.H2OFrame(df, na_strings=['NaN'])
         predictions = self.artifacts.model.predict(h2o_frame)
