d62aaf5341d7c29f9c9ff683012c69620deca87e
bojiang <hrmthw@outlook.com>
2020-08-04 10:10:26 +0800

    Revise lightbgm titanic-survival-prediction example

--- lightbgm/titanic-survival-prediction/lightbgm_titanic_bento_service.py
+++ lightbgm/titanic-survival-prediction/lightbgm_titanic_bento_service.py
@@ -3,13 +3,13 @@ import lightgbm as lgb
 
 import bentoml
 from bentoml.artifact import LightGBMModelArtifact
-from bentoml.handlers import DataframeHandler
+from bentoml.adapters import DataframeInput
 
 @bentoml.artifacts([LightGBMModelArtifact('model')])
 @bentoml.env(pip_dependencies=['lightgbm'])
 class TitanicSurvivalPredictionService(bentoml.BentoService):
     
-    @bentoml.api(DataframeHandler)
+    @bentoml.api(input=DataframeInput())
     def predict(self, df):
         data = df[['Pclass', 'Age', 'Fare', 'SibSp', 'Parch']]
         return self.artifacts.model.predict(data)
