f0ff37a259c35cf5236e2f8627234a6f3a5b9621
Bozhao <yubz86@gmail.com>
2020-09-12 19:59:58 -0700

    update import artifact to new syntax (#53)

--- h2o/loan-prediction/loan_prediction.py
+++ h2o/loan-prediction/loan_prediction.py
@@ -2,14 +2,18 @@
 import h2o
 
 from bentoml import api, env, artifacts, BentoService
-from bentoml.artifact import H2oModelArtifact
+from bentoml.frameworks.h2o import H2oModelArtifact
 from bentoml.adapters import DataframeInput
 
-@env(pip_dependencies = ['h2o', 'pandas'])
+@env(
+    pip_dependencies=['h2o==3.24.0.2', 'pandas'],
+    conda_channels=['h2oai'],
+    conda_dependencies=['h2o==3.24.0.2']
+)
 @artifacts([H2oModelArtifact('model')])
 class LoanPrediction(BentoService):
     
-    @api(input=DataframeInput)
+    @api(input=DataframeInput())
     def predict(self, df):
         h2o_frame = h2o.H2OFrame(df, na_strings=['NaN'])
         predictions = self.artifacts.model.predict(h2o_frame)
