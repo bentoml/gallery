f41b3a6e71fa65abbebf742e252e12dedd7db938
yubozhao <yubz86@gmail.com>
2020-09-22 15:48:27 -0700

    update to 0.9.0

--- xgboost/titanic-survival-prediction/xgboost_titanic_bento_service.py
+++ xgboost/titanic-survival-prediction/xgboost_titanic_bento_service.py
@@ -2,14 +2,14 @@
 import xgboost as xgb
 
 import bentoml
-from bentoml.artifact import XgboostModelArtifact
+from bentoml.frameworks.xgboost import XgboostModelArtifact
 from bentoml.adapters import DataframeInput
 
-@bentoml.env(auto_pip_dependencies=True)
+@bentoml.env(infer_pip_packages=True)
 @bentoml.artifacts([XgboostModelArtifact('model')])
 class TitanicSurvivalPredictionXgBoost(bentoml.BentoService):
     
-    @bentoml.api(input=DataframeInput())
+    @bentoml.api(input=DataframeInput(), batch=True)
     def predict(self, df):
         data = xgb.DMatrix(data=df[['Pclass', 'Age', 'Fare', 'SibSp', 'Parch']])
         return self.artifacts.model.predict(data)
