e48a63c2b302fa88849e577b1bf9a1946a8493d2
eric-roland <eric.roland@gmail.com>
2021-04-21 14:28:42 -0400

    use fastai_image image variable for prediction/update output file

--- fast-ai/pet-image-classification/pet_classification.py
+++ fast-ai/pet-image-classification/pet_classification.py
@@ -1,7 +1,13 @@
 
+import imageio
+import numpy as np
+
 from bentoml import BentoService, api, env, artifacts
 from bentoml.artifact import FastaiModelArtifact
 from bentoml.adapters import FastaiImageInput
+
+from fastai.vision import *
+from fastai.vision import Image, pil2tensor
 
 @env(pip_dependencies=['fastai'])
 @artifacts([FastaiModelArtifact('pet_classifer')])
@@ -9,5 +15,7 @@ class PetClassification(BentoService):
     
     @api(input=FastaiImageInput())
     def predict(self, image):
-        result = self.artifacts.pet_classifer.predict(image)
+        fastai_image = pil2tensor(image, np.float32)
+        fastai_image = Image(fastai_image)
+        result = self.artifacts.pet_classifer.predict(fastai_image)
         return str(result)
