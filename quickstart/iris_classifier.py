bf7d88d499f44ffb5b6faf307b4d4e52df5eb463
Chaoyu <paranoyang@gmail.com>
2021-12-06 18:33:07 -0800

    quickstart update

---
+++ quickstart/iris_classifier.py
@@ -0,0 +1,14 @@
+import numpy as np
+import bentoml
+import bentoml.sklearn
+from bentoml.io import NumpyNdarray
+
+
+iris_clf_runner = bentoml.sklearn.load_runner("iris_clf:latest")
+
+svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
+
+
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
+def classify(input_series: np.ndarray) -> np.ndarray:
+    return iris_clf_runner.run(input_series)
---
+++ quickstart/train.py
@@ -0,0 +1,16 @@
+from sklearn import svm, datasets
+
+import bentoml
+import bentoml.sklearn
+
+
+if __name__ == "__main__":
+    # Load training data
+    iris = datasets.load_iris()
+    X, y = iris.data, iris.target
+    
+    # Model Training
+    clf = svm.SVC()
+    clf.fit(X, y)
+
+    bentoml.sklearn.save("iris_clf", clf)
