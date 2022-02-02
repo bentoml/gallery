0b7c02618f9a56c75eaf5a2aaa54f2d746c4a805
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-02-02 04:36:19 -0500

    fix: wrong generated jupyter notebook file
    fix: format juypter notebooks
    
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- templates/bentoml_gallery_{{cookiecutter.framework.lower().replace('-', '_').replace(' ', '_').replace('scikit_learn','sklearn')}}/model.py
+++ templates/bentoml_gallery_{{cookiecutter.framework.lower().replace('-', '_').replace(' ', '_').replace('scikit_learn','sklearn')}}/model.py
@@ -1,5 +1,5 @@
 # Contributed by {{cookiecutter.author}}.
-"""{{cookiecutter.framework}} {{cookiecutter.project_name}} model implementation."""
+# {{cookiecutter.framework}} {{cookiecutter.project_name}} model implementation.
 
 import {{ cookiecutter.framework.lower().replace(' ', '').replace('pytorch', 'torch') }}
 
