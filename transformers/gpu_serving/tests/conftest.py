0daca76257480afbd4d3fadc7819ac04b32e8a98
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-02-26 20:09:26 -0500

    feat(wip): GPU serving with transformers
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

---
+++ transformers/gpu_serving/tests/conftest.py
@@ -0,0 +1,41 @@
+# pylint: disable=redefined-outer-name
+# type: ignore[no-untyped-def]
+
+import typing as t
+
+import pytest
+from bentoml.testing.server import run_api_server
+
+
+def pytest_configure(config):  # pylint: disable=unused-argument
+    import bentoml
+    import transformers
+
+    FT_NAME = "drobert_ft"
+    PRETRAINED = "emotion_distilroberta_base"
+
+    # transformers model #
+    FINETUNE_MODEL = "aarnphm/finetune_emotion_distilroberta"
+    PRETRAINED_MODEL = "j-hartmann/emotion-english-distilroberta-base"
+
+    m1 = transformers.AutoModelForSequenceClassification.from_pretrained(FINETUNE_MODEL)
+    t1 = transformers.AutoTokenizer.from_pretrained(FINETUNE_MODEL)
+    _ = bentoml.transformers.save(FT_NAME, m1, tokenizer=t1)
+
+    m2 = transformers.AutoModelForSequenceClassification.from_pretrained(
+        PRETRAINED_MODEL
+    )
+    t2 = transformers.AutoTokenizer.from_pretrained(PRETRAINED_MODEL)
+    _ = bentoml.transformers.save(PRETRAINED, m2, tokenizer=t2)
+
+
+@pytest.fixture(scope="session")
+def host() -> t.Generator[str, None, None]:
+    import sys
+    import subprocess
+
+    cmd = f"{sys.executable} -m bentoml build"
+    subprocess.run(cmd, shell=True, check=True)
+
+    with run_api_server(bento="gpu_serving_ft:latest") as host:
+        yield host
