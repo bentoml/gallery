61cb255a2d9f6c7fa23d33d3da98c8ddd93664dc
Chaoyu <paranoyang@gmail.com>
2022-05-06 01:11:20 -0700

    Update locustfile.py

--- quickstart/locustfile.py
+++ quickstart/locustfile.py
@@ -4,13 +4,17 @@ from locust import HttpUser, task, between
 
 test_data = datasets.load_iris().data
 num_of_rows = test_data.shape[0]
+max_batch_size = 10
 
 
 class IrisLoadTestUser(HttpUser):
 
     @task
     def classify(self):
-        input_data = test_data[np.random.choice(num_of_rows)]
-        self.client.post("/classify", json=list(input_data))
+        start = np.random.choice(num_of_rows - max_batch_size)
+        end = start + np.random.choice(max_batch_size)
+
+        input_data = test_data[start:end]
+        self.client.post("/classify", json=input_data.tolist())
 
     wait_time = between(0.01, 2)
