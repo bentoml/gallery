0c04d7fd0f2c9b767a71ed805d8297244ed8126f
bojiang <5886138+bojiang@users.noreply.github.com>
2022-06-02 22:24:04 +0800

    chore: update tensorflow2 for rc0 (#121)

--- tensorflow2/service.py
+++ tensorflow2/service.py
@@ -3,14 +3,8 @@ import numpy as np
 import PIL.Image
 from bentoml.io import Image, NumpyNdarray
 
-mnist_runner = bentoml.tensorflow.load_runner("tensorflow_mnist:latest")
-
-svc = bentoml.Service(
-    name="tensorflow_mnist_demo",
-    runners=[
-        mnist_runner,
-    ],
-)
+mnist_runner = bentoml.tensorflow.get("tensorflow_mnist:latest").to_runner()
+svc = bentoml.Service(name="tensorflow_mnist_demo", runners=[mnist_runner])
 
 
 @svc.api(
@@ -21,17 +15,16 @@ async def predict_ndarray(inp: "np.ndarray") -> "np.ndarray":
     assert inp.shape == (28, 28)
     # We are using greyscale image and our PyTorch model expect one
     # extra channel dimension
-    inp = np.expand_dims(inp, 2)
+    inp = np.expand_dims(inp, (0, 3))
     return await mnist_runner.async_run(inp)
 
 
 @svc.api(input=Image(), output=NumpyNdarray(dtype="float32"))
-async def predict_image(f: PIL.Image) -> "np.ndarray":
-    assert isinstance(f, PIL.Image)
-    arr = np.array(f) / 255.0
+async def predict_image(f: PIL.Image.Image) -> np.ndarray:
+    assert isinstance(f, PIL.Image.Image)
+    arr = np.array(f, dtype="float32") / 255.0
     assert arr.shape == (28, 28)
-
     # We are using greyscale image and our PyTorch model expect one
     # extra channel dimension
-    arr = np.expand_dims(arr, 2).astype("float32")
+    arr = np.expand_dims(arr, (0, 3))
     return await mnist_runner.async_run(arr)
